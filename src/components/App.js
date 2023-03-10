1b849d62886a96f98b82d7a0791ec87d08dc85f5
deepak7691 <kawliya11@gmail.com>
2023-03-11 01:25:30 +0530

    deepak

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,62 @@
+// import React from "react";
+// import "../styles/App.css";
+// import Loader from "./Loader";
+
+// const LoadingStatus = {
+//   NOT_STARTED: "NOT_STARTED",
+//   IN_PROGRESS: "IN_PROGRESS",
+//   SUCCESS: "SUCCESS",
+// };
+
+// const App = () => {
+//   const BASE_URL = "https://content.newtonschool.co/v1/pr/main/users";
+//   const [userId, setUserId] = React.useState(1);
+//   const [isLoading, setIsLoading] = React.useState(LoadingStatus.NOT_STARTED);
+//   const [userData, setUserData] = React.useState({
+//     id: "",
+//     email: "",
+//     name: "",
+//     phone: "",
+//     webiste: "",
+//   });
+
+//   const handleOnClick = () => {};
+
+//   const onChangeHandler = (event) => {
+//     setUserId(event.target.value);
+//   };
+
+//   return (
+//     <div id="main">
+//       <label htmlFor="number">Enter an id for the user between 1 to 100</label>
+//       <input
+//         type="number"
+//         value={userId}
+//         onChange={onChangeHandler}
+//         id="input"
+//         min={1}
+//         max={10}
+//       />
+//       <button id="btn" onClick={handleOnClick}>
+//         Get User
+//       </button>
+
+//       <div id="data">
+//         <h1>Click on the button to get the user</h1>
+//         <h4 id="id">{userData.id}</h4>
+//         <h4 id="email">{userData.email}</h4>
+//         <h4 id="name">{userData.name}</h4>
+//         <h4 id="phone">{userData.phone}</h4>
+//         <h4 id="website">{userData.website}</h4>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+
+
 import React from "react";
 import "../styles/App.css";
 import Loader from "./Loader";
@@ -20,7 +79,19 @@ const App = () => {
     webiste: "",
   });
 
-  const handleOnClick = () => {};
+  const handleOnClick = () => {
+  setIsLoading(LoadingStatus.IN_PROGRESS);
+    setTimeout(() => {
+      fetch(`${BASE_URL}/${userId}`)
+       .then((response)=>response.json())
+       .then((data)=>{
+        setUserData(data);
+        setIsLoading(LoadingStatus.SUCCESS);
+        console.log(userData);  
+       })
+      
+    }, 2000);
+  };
 
   const onChangeHandler = (event) => {
     setUserId(event.target.value);
@@ -40,7 +111,9 @@ const App = () => {
       <button id="btn" onClick={handleOnClick}>
         Get User
       </button>
-
+    {
+      isLoading === LoadingStatus.NOT_STARTED ? <h1>Click on the button to get thr user</h1> :
+      isLoading ===LoadingStatus.IN_PROGRESS ? (<Loader/>):(
       <div id="data">
         <h1>Click on the button to get the user</h1>
         <h4 id="id">{userData.id}</h4>
@@ -49,6 +122,7 @@ const App = () => {
         <h4 id="phone">{userData.phone}</h4>
         <h4 id="website">{userData.website}</h4>
       </div>
+      )}
     </div>
   );
 };
